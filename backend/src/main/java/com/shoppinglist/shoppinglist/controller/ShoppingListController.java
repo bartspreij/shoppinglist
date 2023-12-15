b441e214dce36511ff0dabfb552df11cacc6a5fa
Bart Spreij <bartspreij@gmail.com>
2023-12-15 15:16:54 +0100

    Backend works

---
+++ backend/src/main/java/com/shoppinglist/shoppinglist/controller/ShoppingListController.java
@@ -0,0 +1,23 @@
+package com.shoppinglist.shoppinglist.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("api/v1/shopping-list-items")
+public class ShoppingListController {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+                "boter",
+                "kaas",
+                "eieren"
+        );
+    }
+}
