5bbb24d60b7bbe25508d4660ed82336c4957e572
Bart Spreij <bartspreij@gmail.com>
2023-12-15 15:30:48 +0100

    Frontend works

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+
+const App = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/shopping-list-items")
+    .then(result => result.json())
+    .then(setItems)
+  });
+
+  return <ul>{items.map(item => <li key={item}>{item}</li>)}</ul>
+}
+
+export default App
